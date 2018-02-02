85a60f6089c35ef0035d4d2699dc966597e373a9
Andrew Murray <darbyguitars@gmail.com>
2018-02-02 16:48:04 -0600

    added some jsx to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
-import '../styles/components/Contact.css'
+import React, { Component } from 'react';
+import '../styles/components/Contact.css';
 
 export default class About extends Component {
-  render(){
-    return (
-
-    )
-  }
+	render() {
+		return (
+			<div id="Contact">
+				<h6>
+					<a href="https://github.com/Murray918">GitHub</a>
+				</h6>
+				<h6>
+					<a href="mailto:kamurray1@mac.com.com?Subject=Hello%20again">Email</a>
+				</h6>
+				<h6>
+					<a href="https://www.linkedin.com/in/murray918/">LinkedIn</a>
+				</h6>
+				<h6>
+					<a href="">Resumé</a>
+				</h6>
+			</div>
+		);
+	}
 }
