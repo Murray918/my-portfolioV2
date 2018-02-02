977a645f569f46097680b73bfc3ce14319d451de
Andrew Murray <darbyguitars@gmail.com>
2018-02-02 17:06:23 -0600

    adding some more code and styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,19 @@ export default class About extends Component {
 	render() {
 		return (
 			<div id="Contact">
+				<h1>Contact Me</h1>
+				<hr />
 				<h6>
+					GitHub :
 					<a href="https://github.com/Murray918">GitHub</a>
 				</h6>
 				<h6>
+					{' '}
+					Email :
 					<a href="mailto:kamurray1@mac.com.com?Subject=Hello%20again">Email</a>
 				</h6>
 				<h6>
+					LinkedIn :
 					<a href="https://www.linkedin.com/in/murray918/">LinkedIn</a>
 				</h6>
 				<h6>
